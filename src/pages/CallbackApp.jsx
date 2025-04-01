6c91cc7992112f51b129bdc2a5cbda1895c8fb5f
GeekJulia <juliayaks148@gmail.com>
2025-04-01 13:16:38 +0100

    Updated the callBack file

---
+++ src/pages/CallbackApp.jsx
@@ -0,0 +1,31 @@
+import {useState, useCallback} from "react";
+
+function Counter({increment}){
+    console.log("Counter Component Rendered!");
+
+    return(
+        <div>
+            <button onClick={increment}>Increment</button>
+        </div>
+    );
+}
+export default function CallbackApp(){
+    const [count,setCount] = useState(0)
+    const [text,setText] = useState('')
+
+    //useCallBack memoizes the increment function
+    const increment = useCallback (() => {
+        setCount(count + 1);
+    },[count]); //only re-create if count changes
+
+    return(
+        <>
+        <input value = {text}
+            onChange = {(e) => setText(e.target.value)}
+            placeholder = "Type Something"
+        />
+        <p>Count : {count}</p>
+        <Counter increment = {increment} />
+        </>
+    );
+}
